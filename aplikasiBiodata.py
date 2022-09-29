2b36008e2a165f11d0c31fd1541fad0c708ef6c8
mhmdrflyy <114544930+mhmdrflyy@users.noreply.github.com>
2022-09-28 21:13:29 -0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,22 @@
+# Aplikasi Biodata buatan By Muhammad Rafly
+
+# Memasukan nama
+nama = input("masukan nama anda!")\
+#output nama
+print("Nama anda adalah; ",nama)
+#input kelas
+kelas = input("Masukan Kelas?")
+#output kelas
+print("Kelas anda adalah; ", kelas)
+#input nim
+nim = input("masukan nim!")
+#output nim
+print("nim anda adalah; ", nim)
+#input prodi
+prodi = input("Masukan Prodi!")
+#output prodi
+print("Anda dari prodi; ", prodi)
+#input tanggal lahir
+TanggalLahir = input("Masukan tanggal lahir")
+#output tanggal lahir
+print("Tanggal lahir anda adalah;", TanggalLahir)
