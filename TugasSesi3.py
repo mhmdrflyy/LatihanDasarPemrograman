f98761b653b5c99631cab3ff10af44833f86a654
mhmdrflyy <114544930+mhmdrflyy@users.noreply.github.com>
2022-10-06 09:04:30 +0700

    Add files via upload

---
+++ TugasSesi3.py
@@ -0,0 +1,8 @@
+#Aplikasi biodata sederhana
+
+Nama = "Muhammad Rafly"
+Kelas = "TI22A"
+Jurusan = "Teknik Informatika"
+Quotes = "Key of succes adalah kunci kesuksesan"
+
+print(Nama, Kelas, Jurusan, Quotes)
